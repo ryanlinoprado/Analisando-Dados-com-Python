5704ce40b981d8b3422b6c9cbe0758ecc870f81c
Ryan Prado <ryanlinoprado3@gmail.com>
2026-03-05 15:10:38 -0300

    Update teste.py
    tirando a primeira coluna que não seria útil

--- teste.py
+++ teste.py
@@ -1,14 +1,19 @@
-import pandas
+import pandas as pd
 
 # Coisas Importantes - Passo a Passo
 # 1. Abrir a base de dados.
 # pip install pandas openpyxl nbformat ipykernel plotly
 
-tabela = pandas.read_csv("cancelamentos.csv")
-print(tabela)
+tabela = pd.read_csv("cancelamentos.csv")
+
 
 # 2. Visualizar a base de dados.
     # entender as informações e observar erros
+    # informações inúteis, não ajudam você.
+
+tabela = tabela.drop(columns="CustomerID")
+print(tabela)
+
 # 3. Corrigir problemas da base de dados.
 # 4. Análise inicial (entender quantos clientes cancelaram)
 # 5. Análise detalhada
