5b0e72c3248f58821223568a1714460f858cea22
Ryan Lino Prado <ryanlinoprado3@gmail.com>
2026-03-05 14:06:44 -0300

    verificando dados parte 1

---
+++ teste.py
@@ -0,0 +1,14 @@
+import pandas
+
+# Coisas Importantes - Passo a Passo
+# 1. Abrir a base de dados.
+# pip install pandas openpyxl nbformat ipykernel plotly
+
+tabela = pandas.read_csv("cancelamentos.csv")
+print(tabela)
+
+# 2. Visualizar a base de dados.
+    # entender as informações e observar erros
+# 3. Corrigir problemas da base de dados.
+# 4. Análise inicial (entender quantos clientes cancelaram)
+# 5. Análise detalhada
